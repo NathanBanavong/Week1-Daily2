84b0b9a40c8d4e1ad3d4e57d8c08986946f36f1c
NathanBanavong <nathan.banavong@mobileappscompany.us>
2018-10-24 18:09:24 -0400

    Inserted Class for Exercise

---
+++ app/src/main/java/com/hfad/week1_daily2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.week1_daily2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/hfad/week1_daily2/Week1_Daily2.java
@@ -0,0 +1,4 @@
+package com.hfad.week1_daily2;
+
+public class Week1_Daily2 {
+}
